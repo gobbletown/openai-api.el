ab353e8ee0c64bd619424aa74212727df7d34d5c
Shane Mulligan <mullikine@gmail.com>
2021-07-25 23:03:51 +1200

    Sun 25 Jul 2021 23:03:51

---
+++ oai-engines.el
@@ -0,0 +1,12 @@
+(require 'memoize)
+
+(defun pen-openai-list-engines ()
+  (let ((engines
+         (pen-str2list
+          (pen-snc "pen-openai api engines.list | jq -r '.data[].id'"))))
+    (if (interactive-p)
+        (etv (pen-list2str engines))
+      engines)))
+(memoize 'pen-openai-list-engines)
+
+(provide 'oai-engines)
